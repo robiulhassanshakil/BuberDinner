de05032e6887fb48f4aeda10d3c32762abc35b1a
Robiul <rhasan@primetechbd.com>
2022-07-21 14:09:04 +0600

    implement clean architecture

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Register(string FirstName, string LastName, string Email, string Password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            FirstName,
+            LastName,
+            Email,
+            "token");
+    }
+    public AuthenticationResult Login(string Email, string Password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            Email,
+            "token");
+    }
+}
---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Register(string FirstName, string LastName, string Email, string Password);
+    AuthenticationResult Login(string Email, string Password);
+}
+
