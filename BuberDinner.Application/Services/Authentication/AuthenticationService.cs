bd30b7f7aa3f72a55703ebd60b019a816d27c6f7
Robiul <rhasan@primetechbd.com>
2022-07-25 21:58:05 +0600

    commit for jwt token genarations

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,32 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
     public AuthenticationResult Register(string FirstName, string LastName, string Email, string Password)
     {
+
+
+        // Check if user already exists
+
+        // Create user (generate unique ID)
+
+        // Create JWT token
+        var userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, FirstName, LastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             FirstName,
             LastName,
             Email,
-            "token");
+            token);
     }
     public AuthenticationResult Login(string Email, string Password)
     {
